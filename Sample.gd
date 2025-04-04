4f53d01452dff38b23bca56ff4a3ccbf2042de85
greenpixels <Sven.Peschau@outlook.com>
2025-04-04 17:47:24 +0200

    start migrating to 4.4

--- Sample.gd
+++ Sample.gd
@@ -1,19 +1,35 @@
-extends TextureRect
+extends Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var file_exchange = $Exchange
+@onready var texture_rect = %TextureRect
+func _ready() -> void:
+	file_exchange.binary_loaded.connect(_on_file_loaded)
 
-func _on_UploadButton_pressed():
-	HTML5File.load_image()
-
-	var image = yield(HTML5File, "load_completed")
-
-	var tex = ImageTexture.new()
-	tex.create_from_image(image, 0)
+func _on_file_loaded(buffer: PackedByteArray, file_type: String, file_name: String) -> void:
+	var image = Image.new()
+	var error: Error
 	
-	texture = tex;
+	match file_type:
+		"image/png": error = image.load_png_from_buffer(buffer)
+		"image/jpeg": error = image.load_jpg_from_buffer(buffer)
+		"image/webp": error = image.load_webp_from_buffer(buffer)
+		_:
+			print("Unsupported image format: ", file_type)
+			return
+	
+	if error != OK:
+		print("Failed to load image")
+		return
+		
+	texture_rect.texture = ImageTexture.create_from_image(image)
 
-func _on_DownloadButton_pressed():
-	var image = texture.get_data()
-	HTML5File.save_image(image, "image.png")
+func _on_download_button_pressed() -> void:
+	if not texture_rect.texture:
+		return
+	var image = texture_rect.texture.get_image()
+	var buffer = image.save_png_to_buffer()
+	file_exchange.save_file(buffer, "image.png")
+
+
+func _on_upload_button_pressed() -> void:
+	file_exchange.load_file()
--- addons/HTML5FileExchange/HTML5FileExchange.gd
+++ addons/HTML5FileExchange/HTML5FileExchange.gd
@@ -1,80 +1,57 @@
 extends Node
 
-signal read_completed
-signal load_completed(image)
+signal file_read_completed(file_type: String, file_name: String)
+signal binary_loaded(buffer: PackedByteArray, file_type: String, file_name: String)
 
-var js_callback = JavaScript.create_callback(self, "load_handler");
-var js_interface;
+var js_callback: JavaScriptObject
+var js_interface: JavaScriptObject
 
-func _ready():
-	if OS.get_name() == "HTML5" and OS.has_feature('JavaScript'):
-		_define_js()
-		js_interface = JavaScript.get_interface("_HTML5FileExchange");
+static var _js_initialized := false
 
-func _define_js()->void:
-	#Define JS script
-	JavaScript.eval("""
-	var _HTML5FileExchange = {};
-	_HTML5FileExchange.upload = function(gd_callback) {
-		canceled = true;
-		var input = document.createElement('INPUT'); 
-		input.setAttribute("type", "file");
-		input.setAttribute("accept", "image/png, image/jpeg, image/webp");
-		input.click();
-		input.addEventListener('change', event => {
-			if (event.target.files.length > 0){
-				canceled = false;}
-			var file = event.target.files[0];
-			var reader = new FileReader();
-			this.fileType = file.type;
-			// var fileName = file.name;
-			reader.readAsArrayBuffer(file);
-			reader.onloadend = (evt) => { // Since here's it's arrow function, "this" still refers to _HTML5FileExchange
-				if (evt.target.readyState == FileReader.DONE) {
-					this.result = evt.target.result;
-					gd_callback(); // It's hard to retrieve value from callback argument, so it's just for notification
-				}
-			}
-		  });
-	}
-	""", true)
+func _ready() -> void:
+	if OS.get_name() == "Web" and JavaScriptBridge.eval("typeof window != 'undefined'"):
+		if not _js_initialized:
+			_init_js()
+		js_callback = JavaScriptBridge.create_callback(load_handler)
+		js_interface = JavaScriptBridge.get_interface("_HTML5FileExchange")
 
-func load_handler(_args):
-	emit_signal("read_completed")
+func _init_js() -> void:
+	var js_path := "res://addons/HTML5FileExchange/exchange.js"
+	if not FileAccess.file_exists(js_path):
+		push_error("JavaScript file not found: " + js_path)
+		return
+		
+	var file := FileAccess.open(js_path, FileAccess.READ)
+	var js_code := file.get_as_text()
+	file.close()
 	
-func load_image():
-	if OS.get_name() != "HTML5" or !OS.has_feature('JavaScript'):
+	JavaScriptBridge.eval(js_code, true)
+	_js_initialized = true
+
+func load_handler(_args) -> void:
+	var file_type = js_interface.fileType
+	var file_name = js_interface.fileName
+	file_read_completed.emit(file_type, file_name)
+
+func load_file() -> void:
+	if OS.get_name() != "Web" or !JavaScriptBridge.eval("typeof window != 'undefined'"):
 		return
 
-	js_interface.upload(js_callback);
+	js_interface.upload(js_callback)
+	
+	var file_type: String
+	var file_name: String
+	await file_read_completed.connect(
+		func(type: String, name: String) -> void:
+			file_type = type
+			file_name = name
+	)
+	
+	#var file_data = JavaScriptBridge.eval("_HTML5FileExchange.result", true)
+	#binary_loaded.emit(file_data, file_type, file_name)
 
-	yield(self, "read_completed")
-	
-	var imageType = js_interface.fileType;
-	var imageData = JavaScript.eval("_HTML5FileExchange.result", true) # interface doesn't work as expected for some reason
-	
-	var image = Image.new()
-	var image_error
-	match imageType:
-		"image/png":
-			image_error = image.load_png_from_buffer(imageData)
-		"image/jpeg":
-			image_error = image.load_jpg_from_buffer(imageData)
-		"image/webp":
-			image_error = image.load_webp_from_buffer(imageData)
-		var invalidType:
-			print("Unsupported file format - %s." % invalidType)
-			return
-	
-	if image_error:
-		print("An error occurred while trying to display the image.")
-	
-	emit_signal("load_completed", image)
-
-func save_image(image:Image, fileName:String = "export.png")->void:
-	if OS.get_name() != "HTML5" or !OS.has_feature('JavaScript'):
+func save_file(buffer: PackedByteArray, file_name: String) -> void:
+	if OS.get_name() != "Web" or !JavaScriptBridge.eval("typeof window != 'undefined'"):
 		return
 	
-	image.clear_mipmaps()
-	var buffer = image.save_png_to_buffer()
-	JavaScript.download_buffer(buffer, fileName)
+	JavaScriptBridge.download_buffer(buffer, file_name)
